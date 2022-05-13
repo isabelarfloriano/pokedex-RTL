a4b20daa35f1f1e0aaa086d099bd3ea59f7ad3da
Isabela Rozani Floriano <irf161295@gmail.com>
2022-05-13 20:28:49 -0300

    Create tests links Home About Favorite Pokemons

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,18 @@
-test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
-  () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
+it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
+  () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritesLink).toBeInTheDocument();
   });

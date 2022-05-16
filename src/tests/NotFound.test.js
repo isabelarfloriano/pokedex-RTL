33a2c5b446bda072155d85921540c8df44ae7dd0
Isabela Rozani Floriano <irf161295@gmail.com>
2022-05-16 19:14:39 -0300

    Create tests from NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+it('Testa conteúdo da página Not Found', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundTitle = screen.getByRole('heading',
+    { name: 'Page requested not found Crying emoji' });
+  expect(notFoundTitle).toBeInTheDocument();
+
+  const notFoundAllImage = screen.getAllByRole('img');
+  const notFoundImage = notFoundAllImage[1];
+  const linkImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(notFoundImage).toHaveAttribute('src', linkImage);
+});

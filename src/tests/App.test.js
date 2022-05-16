dadcf354e45bd804d3e0e9c495bcae13e489e834
Isabela Rozani Floriano <irf161295@gmail.com>
2022-05-16 11:22:29 -0300

    Create tests for routes from App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação.',
   () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
     const homeLink = screen.getByRole('link', { name: 'Home' });
     expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
 
     const aboutLink = screen.getByRole('link', { name: 'About' });
     expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
 
     const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
     expect(favoritesLink).toBeInTheDocument();
+    userEvent.click(favoritesLink);
+
+    history.push('/not-found');
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+    expect(notFoundTitle).toBeInTheDocument();
   });

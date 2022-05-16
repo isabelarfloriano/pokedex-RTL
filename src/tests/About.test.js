bf13a4a94a2b9e2971dc724de34ce5050db6cf81
Isabela Rozani Floriano <irf161295@gmail.com>
2022-05-16 14:07:10 -0300

    Create tests from About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+it('Teste o conteúdo da página About', () => {
+  const { container } = renderWithRouter(<About />);
+
+  const aboutTitle = screen.getByRole('heading',
+    { name: 'About Pokédex' });
+  expect(aboutTitle).toBeInTheDocument();
+
+  // const aboutParagraph = screen.getAllByRole('paragraph');
+  const aboutParagraph = container.querySelectorAll('p');
+  const LinkLength = 2;
+  expect(aboutParagraph).toHaveLength(LinkLength);
+
+  const aboutImage = screen.getByRole('img');
+  const linkImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(aboutImage).toHaveAttribute('src', linkImage);
+});

6a24eca196c83b99eeb45334a3568adf30aa0b30
Isabela Rozani Floriano <irf161295@gmail.com>
2022-07-20 11:45:22 -0300

    feat: upgrade tests for pokemondetails page application

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -19,3 +19,39 @@ it('Teste se as informações detalhadas do pokémon selecionado são rederizada
   const summaryText = screen.getByText(pokemons[0].summary);
   expect(summaryText).toBeInTheDocument();
 });
+it('Teste se existe na página uma seção com os mapas contendo as localizações', () => {
+  renderWithRouter(<App />);
+  const getLinkDetails = screen.getByRole('link', { name: /more details/i });
+  expect(getLinkDetails).toBeInTheDocument();
+  userEvent.click(getLinkDetails);
+  const mapsTitle = screen.getByRole('heading', {
+    level: 2, name: /Game Locations of Pikachu/i });
+  expect(mapsTitle).toBeInTheDocument();
+  const locationOne = screen.getByText(pokemons[0].foundAt[0].location);
+  expect(locationOne).toBeInTheDocument();
+  const locationTwo = screen.getByText(pokemons[0].foundAt[1].location);
+  expect(locationTwo).toBeInTheDocument();
+  const getMaps = screen.getAllByRole('img', {
+    name: /pikachu location/i,
+  });
+  expect(getMaps).toHaveLength(pokemons[0].foundAt.length);
+});
+it('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+  renderWithRouter(<App />);
+  const getLinkDetails = screen.getByRole('link', { name: /more details/i });
+  expect(getLinkDetails).toBeInTheDocument();
+  userEvent.click(getLinkDetails);
+  const favoriteAsk = screen.getByLabelText(/Pokémon favoritado/i);
+  expect(favoriteAsk).toBeInTheDocument();
+  const isFavorite = screen.getByRole('checkbox');
+  expect(isFavorite.id).toBe('favorite');
+  expect(isFavorite.checked).toBe(false);
+  userEvent.click(isFavorite);
+  expect(isFavorite.checked).toBe(true);
+  const favoriteStar = screen.getByRole('img', {
+    name: /Pikachu is marked as favorite/i });
+  expect(favoriteStar).toBeInTheDocument();
+  userEvent.click(isFavorite);
+  expect(isFavorite.checked).toBe(false);
+  expect(favoriteStar).not.toBeInTheDocument();
+});
